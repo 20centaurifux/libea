3eeb8c02e79692c51c7e406a6d84ca47e25fb52f
Sebastian Fedrau <lord-kefir@arcor.de>
2013-01-19 22:55:06 +0100

    implementation of single-swap mutation operator

---
+++ src/SingleSwapMutation.h
@@ -0,0 +1,52 @@
+/***************************************************************************
+    begin........: November 2012
+    copyright....: Sebastian Fedrau
+    email........: lord-kefir@arcor.de
+ ***************************************************************************/
+
+/***************************************************************************
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+    General Public License for more details.
+ ***************************************************************************/
+/*!
+ * \file SingleSwapMutation.h
+ * \brief Mutation operator to swap 2 genes.
+ * \author Sebastian Fedrau <lord-kefir@arcor.de>
+ * \version 0.1.0
+ */
+
+#ifndef SINGLESWAPMUTATION_H
+#define SINGLESWAPMUTATION_H
+
+#include "AMutation.h"
+#include "ARandomNumberGenerator.h"
+
+namespace ea
+{
+	class SingleSwapMutation : public AMutation
+	{
+		public:
+			SingleSwapMutation(ARandomNumberGenerator* rnd_generator) : AMutation(rnd_generator) {}
+			~SingleSwapMutation() {};
+
+			void mutate(ea::Genome* genome)
+			{
+				int32_t offsets[2];
+
+				if(genome->size() <= 1)
+				{
+					return;
+				}
+
+				generator->get_unique_numbers(0, genome->size() - 1, offsets, 2);
+				genome->swap(offsets[0], offsets[1]);
+			}
+	};
+}
+#endif
