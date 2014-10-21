f4280568e03fcf9f24ac0216a9e6097d29e00a94
Sebastian Fedrau <sebastian.fedrau@gmail.com>
2014-10-21 07:04:29 +0200

    added genome test case

--- src/test.cpp
+++ src/test.cpp
@@ -11,15 +11,15 @@
 #include <set>
 #include <algorithm>
 
-#include "AnsiRandomNumberGenerator.hpp"
-#include "TR1UniformDistribution.hpp"
-
 using namespace std;
 using namespace CPPUNIT_NS;
 
 /*
  *	test random number generators:
  */
+#include "AnsiRandomNumberGenerator.hpp"
+#include "TR1UniformDistribution.hpp"
+
 template<typename TRandom>
 class RandomNumberGeneratorTest : public CPPUNIT_NS::TestFixture
 {
@@ -30,7 +30,6 @@ class RandomNumberGeneratorTest : public CPPUNIT_NS::TestFixture
 	CPPUNIT_TEST(test_double_seq);
 	CPPUNIT_TEST(test_unique_int32_seq);
 	CPPUNIT_TEST(test_unique_double_seq);
-
 	CPPUNIT_TEST_SUITE_END();
 
 	protected:
@@ -170,8 +169,186 @@ class RandomNumberGeneratorTest : public CPPUNIT_NS::TestFixture
 typedef RandomNumberGeneratorTest<ea::AnsiRandomNumberGenerator> AnsiRandomNumberGeneratorTest;
 typedef RandomNumberGeneratorTest<ea::TR1UniformDistribution<mt19937_64>> TR1UniformDistributionTest;
 
-CPPUNIT_TEST_SUITE_REGISTRATION(AnsiRandomNumberGeneratorTest);
-CPPUNIT_TEST_SUITE_REGISTRATION(TR1UniformDistributionTest);
+//CPPUNIT_TEST_SUITE_REGISTRATION(AnsiRandomNumberGeneratorTest);
+//CPPUNIT_TEST_SUITE_REGISTRATION(TR1UniformDistributionTest);
+
+/*
+ *	genome tests:
+ */
+#include "PrimitiveGenome.hpp"
+
+template<typename TGenomeBase, typename Factory>
+class GenomeBaseTest : public CPPUNIT_NS::TestFixture
+{
+	typedef GenomeBaseTest<TGenomeBase, Factory> GenomeTestCaseType;
+	CPPUNIT_TEST_SUITE(GenomeTestCaseType);
+	CPPUNIT_TEST(test_genome_base);
+	CPPUNIT_TEST_SUITE_END();
+
+	protected:
+		void test_genome_base()
+		{
+			Factory factory;
+			typename TGenomeBase::gene_type* genes[2];
+			typename TGenomeBase::gene_type not_in_set;
+			typename TGenomeBase::sequence_type sequences[4];
+			uint32_t i;
+
+			for(uint32_t size = 10; size < 10000; size *= 10)
+			{
+				/* create test sequences with same length and genes, but differing
+				   order and hashes */
+				factory.generate_differing_gene_sets(genes, size, not_in_set);
+
+				sequences[0] = _base.create(size);
+				sequences[1] = _base.create(size);
+
+				for(i = 0; i < size; i++)
+				{
+					_base.set(sequences[0], i, genes[0][i]);
+					_base.set(sequences[1], i, genes[1][i]);
+				}
+
+				for(i = 0; i < 2; i++)
+				{
+					// test length:
+					CPPUNIT_ASSERT(_base.len(sequences[i]) == size);
+
+					// get genes from sequence and compare them to genes of generated gene array:
+					test_get(sequences[i], genes[i], size);
+	
+					// test index_of method:
+					test_index_of(sequences[i], genes[i], size, not_in_set);
+
+					// compare hashes:
+					CPPUNIT_ASSERT(_base.hash(sequences[0]) != _base.hash(sequences[1]));
+
+					// compare sequences:
+					CPPUNIT_ASSERT(_base.cmp(sequences[0], sequences[1]) != 0);
+
+					// copy first sequence:
+					auto clone = _base.copy(sequences[0]);
+
+					// compare sequence with clone:
+					CPPUNIT_ASSERT(_base.len(sequences[0]) == _base.len(clone));
+					CPPUNIT_ASSERT(_base.cmp(sequences[0], clone) == 0);
+					CPPUNIT_ASSERT(_base.hash(sequences[0]) == _base.hash(clone));
+
+					// change clone & compare it again to parent sequence:
+					auto gene = _base.get(clone, 0);
+					_base.set(clone, 0, _base.get(clone, size - 1));
+					_base.set(clone, size - 1, gene);
+
+					CPPUNIT_ASSERT(_base.len(sequences[0]) == _base.len(clone));
+					CPPUNIT_ASSERT(_base.cmp(sequences[0], clone) != 0);
+					CPPUNIT_ASSERT(_base.hash(sequences[0]) != _base.hash(clone));
+
+					// destroy clone:
+					_base.dispose(clone);
+				}
+
+				// clean up:
+				for(i = 0; i < 2; i++)
+				{
+					_base.dispose(sequences[i]);
+					delete[] genes[i];
+				}
+			}
+		}
+
+	private:
+		TGenomeBase _base;
+
+		void test_get(typename TGenomeBase::sequence_type& sequence, const typename TGenomeBase::gene_type *genes, const int32_t len)
+		{
+			CPPUNIT_ASSERT(_base.len(sequence) == len);
+
+			for(uint32_t i = 0; i < len; i++)
+			{
+				CPPUNIT_ASSERT(_base.get(sequence, i) == genes[i]);
+			}
+		}
+
+		void test_index_of(typename TGenomeBase::sequence_type& sequence, const typename TGenomeBase::gene_type *genes, const int32_t len, const typename TGenomeBase::gene_type not_in_sequence)
+		{
+			CPPUNIT_ASSERT(_base.len(sequence) == len);
+
+			for(uint32_t i = 0; i < len; i++)
+			{
+				CPPUNIT_ASSERT(_base.index_of(sequence, genes[i]) == i);
+			}
+
+			CPPUNIT_ASSERT(_base.index_of(sequence, not_in_sequence) == -1);
+		}
+};
+
+template<typename TSequence>
+class SumFitness
+{
+	public:
+		float operator()(const TSequence* const& seq) const
+		{
+			float sum = 0;
+
+			for(auto i = 0; i < seq->len; i++)
+			{
+				sum += seq->genes[i];
+			}
+
+			return sum;
+		}
+};
+
+class Int32Factory
+{
+	public:
+		void generate_differing_gene_sets(int32_t* genes[2], uint32_t size, int32_t& not_in_set)
+		{
+			uint32_t i;
+			uint32_t sep;
+
+			genes[0] = new int32_t[size];
+			_rnd.get_unique_int32_seq(0, size - 1, genes[0], size);
+
+			genes[1] = new int32_t[size];
+
+			sep = size / 2;
+
+			for(i = 0; i < sep; i++)
+			{
+				genes[1][i] = genes[0][i + sep];
+			}
+
+			for(i = sep; i < size; i++)
+			{
+				genes[1][i] = genes[0][i - sep];
+			}
+
+			not_in_set = size;
+		}
+
+	private:
+		ea::TR1UniformDistribution<> _rnd;
+};
+
+typedef ea::PrimitiveGenomeBase<int32_t, SumFitness<ea::Sequence<int32_t>>> PrimitiveInt32GenomeBase;
+typedef GenomeBaseTest<PrimitiveInt32GenomeBase, Int32Factory> PrimitiveInt32GenomeBaseTest;
+
+CPPUNIT_TEST_SUITE_REGISTRATION(PrimitiveInt32GenomeBaseTest);
+
+/*
+template<typename TGenomeBase>
+class Int32GenomeBaseTest : public GenomeBaseTest<TGenomeBase>
+{
+	protected:
+		virtual int32_t *create_genes(const size_t size)
+		{
+			return nullptr;
+		}
+};
+*/
+
+//Int32GenomeBaseTest<ea::PrimitiveGenomeBase<int32_t, SumFitness<ea::Sequence<int32_t>>> foo;
 
 int main(int argc, char* argv[])
 {
