d17470700fa83ebdf228d5dd6402b25e95d8885c
Sebastian Fedrau <sebastian.fedrau@gmail.com>
2015-01-21 20:51:51 +0100

    added pipeline example code

---
+++ examples/05-pipeline.cpp
@@ -0,0 +1,119 @@
+#include <libea.hpp>
+#include <vector>
+#include <map>
+
+using namespace std;
+using namespace ea;
+using namespace ea::Pipeline;
+
+// type declarations:
+typedef uint8_t City;
+typedef CSequence<City>* Route;
+
+typedef struct
+{
+	uint16_t x;
+	uint16_t y;
+} Point;
+
+// city map:
+static map<City, Point> Cities = {
+	{ 0, { 31, 9 } },
+	{ 1, { 17, 82 } },
+	{ 2, { 56, 47 } },
+	{ 3, { 70,  3 } },
+	{ 4, { 11,  7 } },
+	{ 5, { 93, 83 } },
+	{ 6, { 96, 22 } },
+	{ 7, { 26, 97 } },
+	{ 8, { 52, 58 } },
+	{ 9, { 68,  6 } } };
+
+// fitness:
+class Fitness
+{
+	public:
+		float operator()(const Route &seq) const
+		{
+			float f = 0;
+
+			for(auto i = 0; i < sequence_len(seq) - 1; i++)
+			{
+				auto a = sequence_get(seq, i);
+				auto b = sequence_get(seq, i + 1);
+
+				f += sqrt(pow(Cities[b].x - Cities[a].x, 2) + pow(Cities[b].y - Cities[a].y, 2));
+			}
+
+			return f;
+		}
+};
+
+// base class type declaration:
+typedef CPGenomeBase<City, Fitness> Base;
+
+// factory:
+class RouteFactory : public ea::AFactory<Route>
+{
+	public:
+		Route create_sequence()
+		{
+			int32_t genes[10];
+
+			_g.get_int32_seq(0, 9, genes, 10);
+
+			auto seq = _base.create(10);
+
+			for(uint32_t i = 0; i < 10; i++)
+			{
+				_base.set(seq, i, genes[i]);
+			}
+
+			return seq;
+		}
+
+	private:
+		static AnsiRandomNumberGenerator _g;
+		static Base _base;
+};
+
+AnsiRandomNumberGenerator RouteFactory::_g;
+Base RouteFactory::_base;
+
+// process pipeline:
+int
+main(int argc, char *argv[])
+{
+	Base base;
+	RouteFactory f;
+	vector<Route> population;
+	auto rnd = make_shared<ea::AnsiRandomNumberGenerator>();
+
+	// create 100 routes:
+	auto inserter = back_inserter(population);
+	STLVectorAdapter<Route> adapter(inserter);
+	f.create_population(100, adapter);
+
+	// create and process pipeline:
+	auto source = make_input_adapter(population);
+
+	vector<Route> children;
+	auto cinserter = back_inserter(children);
+	STLVectorAdapter<Route> cadapter(cinserter);
+
+	auto selection_a = SelectionElement<Base, SourceDivisor<Base>>(new TournamentSelection<Base>());
+	auto selection_b = SelectionElement<Base, FixedSelectionSize<Base, 50>>(new TournamentSelection<Base>());
+	auto crossover = CrossoverElement<Base>(new CutAndSpliceCrossover<Base>());
+	auto mutation = MutationElement<Base>(new SingleSwapMutation<Base>(), rnd);
+	auto terminator = ForLoopTerminator<Base>(10);
+
+	ITerminator<Base>& terminator_ref = terminator;
+
+	pipeline_process<Base>(source, cadapter, { &selection_a, &crossover, &mutation, &selection_b }, terminator_ref);
+
+	// cleanup:
+	dispose(base, begin(population), end(population));
+	dispose(base, begin(children), end(children));
+
+	return 0;
+}
